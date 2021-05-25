6d050437281754816eb06c21d808d54e7e409d15
Gökhan Ertaş <gokhan.ertas@outlook.com>
2021-05-25 22:32:36 +0300

    Telegram Bot Api was impelemted for utilites project

--- CryptoTrader.Utilities/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CryptoTrader.Utilities
-{
-    public class Class1
-    {
-    }
-}
---
+++ CryptoTrader.Utilities/Telegram.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Telegram.Bot;
+
+namespace CryptoTrader.Utilities
+{
+    public static class Telegram
+    {
+        private static TelegramBotClient botClient;
+
+        private static async void SendTextMessageAsync(string token, string chatId, string message)
+        {
+            botClient = new TelegramBotClient(token);
+
+            await botClient.SendTextMessageAsync(chatId, message);
+        }
+    }
+}
